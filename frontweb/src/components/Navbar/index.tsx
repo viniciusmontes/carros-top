f03ac455bf0c6f7f23649cf1c473c9e43388d504
Vinicius Montes <viniciusmontes@outlook.com.br>
2023-03-11 15:01:21 -0300

    Navbar

---
+++ frontweb/src/components/Navbar/index.tsx
@@ -0,0 +1,27 @@
+import './styles.css';
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className="bg-primary">
+        <div>
+          <a href="link">
+            <h4>Carros Top</h4>
+          </a>
+          <div>
+            <ul>
+              <li>
+                <a href="link">HOME</a>
+              </li>
+              <li>
+                <a href="link">CATÁLOGO</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
